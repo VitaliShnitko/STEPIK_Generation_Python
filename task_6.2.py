43c652cbd1de617b57dea6779bd468a3f2405c4d
Vitali Shnitko <stigmator565@gmail.com>
2022-09-29 19:18:45 +0300

    Add solution 2 task.

--- task_6.2.py
+++ task_6.2.py
@@ -2,5 +2,14 @@
 #
 # "Python is a great language!", said Fred. "I don't ever remember having this much fun before."
 
-str_ = '''"Python is a great language!", said Fred. "I don't ever remember having this much fun before."'''
-print(str_)
+# str_ = '''"Python is a great language!", said Fred. "I don't ever remember having this much fun before."'''
+# print(str_)
+
+# Напишите программу, которая считывает с клавиатуры две строки – имя и фамилию пользователя и выводит фразу:
+#
+# «Hello [введенное имя] [введенная фамилия]! You just delved into Python».
+
+name = input()
+family = input()
+
+print('Hello ' + name + ' ' + family + '!' + ' You just delved into Python')
