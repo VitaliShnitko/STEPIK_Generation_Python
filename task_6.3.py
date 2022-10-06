efd6da7ae88497288cfc85808ffbda34d66408cc
Vitali Shnitko <stigmator565@gmail.com>
2022-10-06 20:10:22 +0300

    Add solution 2 task

--- task_6.3.py
+++ task_6.3.py
@@ -1,10 +1,21 @@
 # На плоскости евклидово расстояние между двумя точками(x1;y1) b (x2;y2)
-num1 = float(input())
-num2 = float(input())
-num3 = float(input())
-num4 = float(input())
+# num1 = float(input())
+# num2 = float(input())
+# num3 = float(input())
+# num4 = float(input())
+#
+# p = ((num1 - num3) ** 2 + (num2 - num4) ** 2) ** 0.5
+#
+# print(p)
 
-p = ((num1 - num3) ** 2 + (num2 - num4) ** 2) ** 0.5
+# Напишите программу определяющую площадь круга и длину окружности по заданному радиусу R.
+from math import pi
+r = float(input())
+p = pi
+S = p * (r ** 2)
+C = 2 * p * r
+print(S)
+print(C)
 
-print(p)
+
 
