24ee0b8589c0b2f4275dc14262cf2972bf675ccb
Vitali Shnitko <stigmator565@gmail.com>
2022-10-06 21:47:14 +0300

    Add solution 3 task

--- task_7.1.py
+++ task_7.1.py
@@ -6,8 +6,19 @@
 # Дано предложение и количество раз которое его надо повторить.
 # Напишите программу, которая повторяет данное предложение нужное количество раз.
 
-text = input()
-n = int(input())
+# text = input()
+# n = int(input())
+#
+# for i in range(n):
+#     print(text)
 
-for i in range(n):
-    print(text)
+
+# Напишите программу, которая использует ровно три цикла for для печати следующей последовательности символов:
+for i in range(6):
+    print('AAA')
+for i in range(5):
+    print('BBBB')
+print('E')
+for i in range(9):
+    print('TTTTT')
+print('G')
