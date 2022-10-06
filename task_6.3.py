933c1722a8ac5c1e135a81f8ad46bcf07dd5ca29
Vitali Shnitko <stigmator565@gmail.com>
2022-10-06 20:00:01 +0300

    Add file task_6.3.Add solution 1 task

---
+++ task_6.3.py
@@ -0,0 +1,10 @@
+# На плоскости евклидово расстояние между двумя точками(x1;y1) b (x2;y2)
+num1 = float(input())
+num2 = float(input())
+num3 = float(input())
+num4 = float(input())
+
+p = ((num1 - num3) ** 2 + (num2 - num4) ** 2) ** 0.5
+
+print(p)
+
