94b0d3b38b53d665c868875a6ce094423ac21e20
Vitali Shnitko <stigmator565@gmail.com>
2022-10-12 20:21:28 +0300

    add file task_7.2.py. Add solution 2 task.

--- task_7.2.py
+++ task_7.2.py
@@ -1,8 +1,24 @@
 # Даны два целых числа m и n ( m ≤ n). Напишите программу,
 # которая выводит все числа от m до n включительно.
 
+# m = int(input())
+# n = int(input())
+#
+# for i in range(m, n+1):
+#     print(i)
+
+# Даны два целых числа mm и nn. Напишите программу,
+# которая выводит все числа от m до n включительно в порядке возрастания, если m < n,
+# или в порядке убывания в противном случае.
+
 m = int(input())
 n = int(input())
 
-for i in range(m, n+1):
-    print(i)
+if m < n:
+    for i in range(m, n+1):
+        print(i)
+elif m > n:
+    for i in range(m, n-1, -1):
+        print(i)
+else:
+    print(m)
