4087a8f29e6b779658a46df8a77b1f88342b0eaa
Vitali Shnitko <stigmator565@gmail.com>
2022-10-12 20:50:35 +0300

    add file task_7.2.py. Add solution 3 task.

--- task_7.2.py
+++ task_7.2.py
@@ -11,14 +11,27 @@
 # которая выводит все числа от m до n включительно в порядке возрастания, если m < n,
 # или в порядке убывания в противном случае.
 
+# m = int(input())
+# n = int(input())
+#
+# if m < n:
+#     for i in range(m, n+1):
+#         print(i)
+# elif m > n:
+#     for i in range(m, n-1, -1):
+#         print(i)
+# else:
+#     print(m)
+
+# Даны два целых числа m и n(m > n). Напишите программу,
+# которая выводит все нечетные числа от m до n включительно в порядке убывания.
+
 m = int(input())
 n = int(input())
 
-if m < n:
-    for i in range(m, n+1):
+for i in range(m, n, -2):
+    if m % 2 != 0:
         print(i)
-elif m > n:
-    for i in range(m, n-1, -1):
-        print(i)
-else:
-    print(m)
+    elif m % 2 == 0:
+        print(i-1)
+
