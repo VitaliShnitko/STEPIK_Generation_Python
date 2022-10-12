3dcafe120d4863ad078c7e63710406db5db9c4c1
Vitali Shnitko <stigmator565@gmail.com>
2022-10-12 21:05:36 +0300

    Add solution 5 task.Finish task_7.2.py

--- task_7.2.py
+++ task_7.2.py
@@ -43,11 +43,18 @@
 # число кратно 3 и 5 одновременно.
 
 
-m = int(input())
+# m = int(input())
+# n = int(input())
+#
+# for i in range(m, n+1):
+#     if i%17 == 0 or i%3 == 0 and i%5 == 0 or i%10 == 9:
+#         print(i)
+#
+# Дано натуральное число n. Напишите программу, которая выводит таблицу умножения на n.
+
 n = int(input())
 
-for i in range(m, n+1):
-    if i%17 == 0 or i%3 == 0 and i%5 == 0 or i%10 == 9:
-        print(i)
+for i in range(1, 11):
+    print(n, 'x', i, '=', n * i)
 
 
