49cb7597d4f17c4182dc38e0c7d358d511305a2e
Vitali Shnitko <stigmator565@gmail.com>
2022-10-16 12:05:29 +0300

    Add solution 9 task

--- task_7.3.py
+++ task_7.3.py
@@ -96,16 +96,29 @@
 # каждое на отдельной строке. Напишите программу,
 # которая выводит наибольшее и второе наибольшее число последовательности.
 
-n = int(input())
-max1 = 0
-max2 = 1
-for i in range(1, n+1):
+# n = int(input())
+# max1 = 0
+# max2 = 1
+# for i in range(1, n+1):
+#     num = int(input())
+#     if num > max1:
+#         max2 = max1
+#         max1 = num
+#     elif num > max2:
+#         max2 = num
+# print(max1)
+# print(max2)
+
+# Напишите программу, которая считывает последовательность из 10 целых чисел
+# и определяет является ли каждое из них четным или нет.
+n = 0
+for i in range(10):
     num = int(input())
-    if num > max1:
-        max2 = max1
-        max1 = num
-    elif num > max2:
-        max2 = num
-print(max1)
-print(max2)
+    if num % 2 == 0:
+        n += 1
+if n == 10:
+    print('YES')
+else:
+    print('NO')
+
 
