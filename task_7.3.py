908cb709a7e070d330fa474f7afe94012d4a26ac
Vitali Shnitko <stigmator565@gmail.com>
2022-10-16 11:06:32 +0300

    Add solution 4 task

--- task_7.3.py
+++ task_7.3.py
@@ -26,11 +26,22 @@
 # На вход программе подается натуральное число n. Напишите программу, которая вычисляет значение выражения
 # Примечание. Для вычисления натурального логарифма воспользуйтесь функцией log(n), которая находится в модуле math.
 
-from math import log
+# from math import log
+#
+# n = int(input())
+# counter = 0
+# for i in range(1, n+1):
+#     counter = counter + (1 / i)
+#     num = counter - log(n)
+# print(num)
+
+
+# На вход программе подается натуральное число nn. Напишите программу,
+# которая подсчитывает сумму тех чисел от 1 до n (включительно) квадрат которых оканчивается на 2,5 или 8.
 
 n = int(input())
-counter = 0
+total = 0
 for i in range(1, n+1):
-    counter = counter + (1 / i)
-    num = counter - log(n)
-print(num)
+    if (i**2) % 10 == 2 or (i**2) % 10 == 5 or (i**2) % 10 == 10:
+        total += i
+print(total)
