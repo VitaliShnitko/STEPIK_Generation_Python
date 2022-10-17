f45417297ece12983232cde2247142f319c03ded
Vitali Shnitko <stigmator565@gmail.com>
2022-10-17 20:53:19 +0300

    Add solution 2 task.

--- task_7.5.py
+++ task_7.5.py
@@ -1,8 +1,19 @@
 # Дано натуральное число. Напишите программу, которая выводит его цифры в столбик в обратном порядке.
 
+# num = int(input())
+#
+# while num != 0:
+#     last_digit = num % 10
+#     print(last_digit)
+#     num = num // 10
+
+
+# Дано натуральное число. Напишите программу, которая меняет порядок цифр числа на обратный.
+
 num = int(input())
 
 while num != 0:
     last_digit = num % 10
-    print(last_digit)
+    print(last_digit, end='')
     num = num // 10
+
