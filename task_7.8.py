8239e94a7aec03a0bbf2dbc79d5a9f3aa2c1a5d5
Vitali Shnitko <stigmator565@gmail.com>
2022-10-31 21:31:02 +0300

    Added solution 5 task. Finish task_7.8.py

--- task_7.8.py
+++ task_7.8.py
@@ -39,13 +39,34 @@
 # ***
 # **
 # *
+
+# n = int(input())
+# for i in range(n//2 + 1):
+#     for j in range(i + 1):
+#         print('*', end='')
+#     print()
+# for i in range(n//2, 0, -1):
+#     for j in range(i, 0, -1):
+#         print('*', end='')
+#     print()
+
+
+
+# Дано натуральное число n. Напишите программу,
+# которая печатает численный треугольник в соответствии с примером:
+# 1
+# 22
+# 333
+# 4444
+# 55555
+# ...
+
 n = int(input())
-for i in range(n//2 + 1):
-    for j in range(i + 1):
-        print('*', end='')
+for i in range(1, n+1):
+    for j in range(1, i+1):
+        print(i, end='')
     print()
-for i in range(n//2, 0, -1):
-    for j in range(i, 0, -1):
-        print('*', end='')
-    print()
+
+
+
 
