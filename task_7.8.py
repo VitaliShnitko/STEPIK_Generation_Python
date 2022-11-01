37587c1c6055bddf55f47ee475053d295124e322
Vitali Shnitko <stigmator565@gmail.com>
2022-11-01 20:06:26 +0300

    Added solution for cows =D

--- task_7.8.py
+++ task_7.8.py
@@ -61,11 +61,22 @@
 # 55555
 # ...
 
-n = int(input())
-for i in range(1, n+1):
-    for j in range(1, i+1):
-        print(i, end='')
-    print()
+# n = int(input())
+# for i in range(1, n+1):
+#     for j in range(1, i+1):
+#         print(i, end='')
+#     print()
+
+
+# Имеется 100100 рублей. Сколько быков, коров и телят можно купить на все эти деньги,
+# если плата за быка – 10 рублей, за корову – 5 рублей,
+# за теленка – 0.5 рубля и надо купить 100 голов скота?
+
+for b in range(1, 11):
+    for k in range(1, 21):
+        for t in range(1, 201):
+            if b * 10 + k * 5 + t * 0.5 == 100 and b + k + t == 100:
+                print(b, k, t)
 
 
 
