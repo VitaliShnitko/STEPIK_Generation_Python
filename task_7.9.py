04ae834c2fe9a012fea71d1f3ec67aa4a7d247cc
Vitali Shnitko <stigmator565@gmail.com>
2022-11-02 22:53:02 +0300

    Added solution 3 task.

--- task_7.9.py
+++ task_7.9.py
@@ -19,10 +19,33 @@
 # 1234321
 # 123454321
 
-n = int(input())
-for i in range(1, n+1):
-    for j in range(1, i+1):
-        print(j, end='')
-    for k in range(i-1, 0, -1):
-        print(k, end='')
-    print()
+# n = int(input())
+# for i in range(1, n+1):
+#     for j in range(1, i+1):
+#         print(j, end='')
+#     for k in range(i-1, 0, -1):
+#         print(k, end='')
+#     print()
+
+
+# На вход программе подается два натуральных числа a и b (a< b).
+# Напишите программу, которая находит натуральное
+# число из отрезка [a;b] с максимальной суммой делителей.
+
+a = int(input())
+b = int(input())
+summ_count = 0
+summ_x = 0
+for x in range(a, b+1):
+    count = 0
+    for i in range(1, b+1):
+        if x % i == 0:
+            count += i
+        if count >= summ_count:
+            summ_count = count
+            summ_x = x
+print(summ_x, summ_count)
+
+
+
+
