60f6fc54c1ac2a08662664c9f4e25e399b2948b5
Vitali Shnitko <stigmator565@gmail.com>
2022-11-10 19:35:16 +0300

    Added solution 5 task.

--- 8. Final_work_on_cycles.py
+++ 8. Final_work_on_cycles.py
@@ -66,19 +66,27 @@
 # Известно, что каждая ошибка затрагивает только одну строку
 # и может быть исправлена без изменения других строк.
 
-n = 4
-count = 0
-maximum = -10**8
-for i in range(1, n + 1):
-    x = int(input())
-    if x % 2 != 0:
-        count += 1
-        if x > maximum:
-            maximum = x
-if count > 0:
-    print(count)
-    print(maximum)
-else:
-    print('NO')
+# n = 4
+# count = 0
+# maximum = -10**8
+# for i in range(1, n + 1):
+#     x = int(input())
+#     if x % 2 != 0:
+#         count += 1
+#         if x > maximum:
+#             maximum = x
+# if count > 0:
+#     print(count)
+#     print(maximum)
+# else:
+#     print('NO')
 
 
+# На вход программе подается натуральное число n.
+# Напишите программу, которая печатает звездную рамку размерами n×19.
+
+n = int(input())
+print('*'*19)
+for i in range(n-2):
+    print('*' + ' ' * 17 + '*')
+print('*'*19)
