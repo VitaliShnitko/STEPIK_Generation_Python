45dbf5cb32491729311aa2d446321a59992bc3b4
Vitali Shnitko <stigmator565@gmail.com>
2022-11-13 20:38:33 +0300

    Added solution 8 task.Finish task_9.2_slices.py

--- task_9.2_slices.py
+++ task_9.2_slices.py
@@ -70,21 +70,30 @@
 # все символы в обратном порядке;
 # все символы строки через один в обратном порядке, начиная с последнего.
 
-text = input()
+# text = input()
+#
+# trird_symbol_text = text[2:3]
+# penultimate_symbol_text = text[-2:-1]
+# first_5_symbol_text = text[:5]
+# del_last_2_symbol = text[:-2]
+# symbol_even_index_text = text[::2]
+# symbol_odd_index_text = text[1::2]
+# revers_text = text[::-1]
+# del_1_revers_text = text[-1::-2]
+#
+# print(trird_symbol_text, penultimate_symbol_text, first_5_symbol_text,
+#       del_last_2_symbol, symbol_even_index_text, symbol_odd_index_text,
+#       revers_text, del_1_revers_text, sep='\n')
 
-trird_symbol_text = text[2:3]
-penultimate_symbol_text = text[-2:-1]
-first_5_symbol_text = text[:5]
-del_last_2_symbol = text[:-2]
-symbol_even_index_text = text[::2]
-symbol_odd_index_text = text[1::2]
-revers_text = text[::-1]
-del_1_revers_text = text[-1::-2]
 
-print(trird_symbol_text, penultimate_symbol_text, first_5_symbol_text,
-      del_last_2_symbol, symbol_even_index_text, symbol_odd_index_text,
-      revers_text, del_1_revers_text, sep='\n')
+# На вход программе подается строка текста.
+# Напишите программу, которая разрежет ее на две равные части,
+# переставит их местами и выведет на экран.
 
 
+word = input()
+half1 = word[:int((len(word)+1)/2)]
+half2 = word[int((len(word)+1)/2):]
+print(half2 + half1)
 
 
