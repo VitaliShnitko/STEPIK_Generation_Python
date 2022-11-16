42bcebfc50c3174035520edfe5ab96d93f760243
Vitali Shnitko <stigmator565@gmail.com>
2022-11-16 22:01:53 +0300

    Added solution 2 task.Finish task_9_5_string_mathods_part_3.py

--- task_9_5_string_mathods_part_3.py
+++ task_9_5_string_mathods_part_3.py
@@ -3,8 +3,18 @@
 # «In 2010, someone paid 10k Bitcoin for two pizzas.» (без кавычек).
 
 
-s = 'In {0}, someone paid {1} {2} for two pizzas.'
-year = '2010'
-summa = '10k'
-cripta = 'Bitcoin'
-print(f"In {year}, someone paid {summa} {cripta} for two pizzas.")
+# s = 'In {0}, someone paid {1} {2} for two pizzas.'
+# year = '2010'
+# summa = '10k'
+# cripta = 'Bitcoin'
+# print(f"In {year}, someone paid {summa} {cripta} for two pizzas.")
+
+
+# Дополните приведенный код, используя форматирование строк с помощью f-строк, так чтобы он вывел текст:
+# «In 2010, someone paid 10K Bitcoin for two pizzas.» (без кавычек).
+
+year = 2010
+amount = '10K'
+currency = 'Bitcoin'
+
+print(f'In {year}, someone paid {amount} {currency} for two pizzas.')
