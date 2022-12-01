49269fadd7b1013ccdbb73677e21d31b5218f582
Vitali Shnitko <stigmator565@gmail.com>
2022-12-01 20:35:58 +0300

    Added solution 2 task.

--- task_13_2_func_parametrers.py
+++ task_13_2_func_parametrers.py
@@ -5,19 +5,38 @@
 # а затем выводит его.
 
 # объявление функции
-def draw_triangle(fill, base):
-    for i in range(base // 2 + 1):
-        for j in range(i + 1):
-            print(fill, end='')
-        print()
-    for i in range(base // 2, 0, -1):
-        for j in range(i, 0, -1):
-            print(fill, end='')
-        print()
+# def draw_triangle(fill, base):
+#     for i in range(base // 2 + 1):
+#         for j in range(i + 1):
+#             print(fill, end='')
+#         print()
+#     for i in range(base // 2, 0, -1):
+#         for j in range(i, 0, -1):
+#             print(fill, end='')
+#         print()
+#
+# # считываем данные
+# fill = input()
+# base = int(input())
+#
+# # вызываем функцию
+# draw_triangle(fill, base)
+
+
+# Напишите функцию print_fio(name, surname, patronymic), которая принимает три параметра:
+#
+# name – имя человека;
+# surname – фамилия человека;
+# patronymic – отчество человека;
+# а затем выводит на печать ФИО человека.
+
+
+# объявление функции
+def print_fio(name, surname, patronymic):
+    print(surname[0].upper(), name[0].upper(), patronymic[0].upper(), sep='')
 
 # считываем данные
-fill = input()
-base = int(input())
+name, surname, patronymic = input(), input(), input()
 
 # вызываем функцию
-draw_triangle(fill, base)
+print_fio(name, surname, patronymic)
