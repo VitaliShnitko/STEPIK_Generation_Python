78277623d515f6af54904f98b43324650585f1f4
Vitali Shnitko <stigmator565@gmail.com>
2022-12-04 13:26:58 +0300

    Added solution 3 task.

--- task_13_4_func_return_part_1.py
+++ task_13_4_func_return_part_1.py
@@ -17,18 +17,37 @@
 # которая принимает в качестве аргумента номер месяца
 # и возвращает количество дней в данном месяце.
 
+# # объявление функции
+# def get_days(month):
+#     if month in [1, 3, 5, 7, 8, 10, 12]:
+#         return 31
+#     if month in [4, 6, 9, 11]:
+#         return 30
+#     else:
+#         return 28
+#
+# # считываем данные
+# num = int(input())
+#
+# # вызываем функцию
+# print(get_days(num))
+
+
+# Напишите функцию get_factors(num),
+# принимающую в качестве аргумента натуральное число и
+# возвращающую список всех делителей данного числа.
+
 # объявление функции
-def get_days(month):
-    if month in [1, 3, 5, 7, 8, 10, 12]:
-        return 31
-    if month in [4, 6, 9, 11]:
-        return 30
-    else:
-        return 28
+def get_factors(num):
+    delitel = []
+    for i in range(1, num+1):
+        if num % i == 0:
+            delitel.append(i)
+    return delitel
 
 # считываем данные
-num = int(input())
+n = int(input())
 
 # вызываем функцию
-print(get_days(num))
+print(get_factors(n))
 
