902723dbd948d20cea4c6583cb180914bb2f5d25
Vitali Shnitko <stigmator565@gmail.com>
2022-12-04 14:12:21 +0300

    Added solution 7 task.

--- task_13_4_func_return_part_1.py
+++ task_13_4_func_return_part_1.py
@@ -100,14 +100,42 @@
 # которая принимает в качестве аргументов два отсортированных по возрастанию списка,
 # состоящих из целых чисел, и объединяет их в один отсортированный список.
 
-# объявление функции
-def merge(list1, list2):
-    l = sorted(list1 + list2)
-    return l
+# # объявление функции
+# def merge(list1, list2):
+#     l = sorted(list1 + list2)
+#     return l
+#
+# # считываем данные
+# numbers1 = [int(c) for c in input().split()]
+# numbers2 = [int(c) for c in input().split()]
+#
+# # вызываем функцию
+# print(merge(numbers1, numbers2))
 
-# считываем данные
-numbers1 = [int(c) for c in input().split()]
-numbers2 = [int(c) for c in input().split()]
 
-# вызываем функцию
-print(merge(numbers1, numbers2))
+# На вход программе подается число n, а затем n строк,
+# содержащих целые числа в порядке возрастания.
+# Из данных строк формируются списки чисел. Напишите программу,
+# которая объединяет указанные списки в один отсортированный
+# список с помощью функции quick_merge(), а затем выводит его.
+
+def quick_merge(lst1, lst2):
+    res = []
+    p1, p2 = 0, 0
+    while p1<len(lst1) and p2<len(lst2):
+        if lst1[p1] < lst2[p2]:
+            res.append(lst1[p1])
+            p1+=1
+        else:
+            res.append(lst2[p2])
+            p2+=1
+    if p1==len(lst1):
+        res += lst2[p2:]
+    else:
+        res += lst1[p1:]
+    return res
+res = []
+for _ in range(int(input())):
+    num = [int(c) for c in input().split()]
+    res = quick_merge(res, num)
+print(*res)
