685adc6509750933951359a2e18cbe13f88e0f46
Vitali Shnitko <stigmator565@gmail.com>
2022-12-06 22:12:30 +0300

    Added solution 2 task.

--- task_13_5_func_return_part_2_bool.py
+++ task_13_5_func_return_part_2_bool.py
@@ -3,15 +3,39 @@
 # если существует невырожденный треугольник со
 # сторонами side1, side2, side3 и False в противном случае.
 
+# # объявление функции
+# def is_valid_triangle(side1, side2, side3):
+#     if a + b > c and a + c > b and b + c > a:
+#         return True
+#     else:
+#         return False
+#
+# # считываем данные
+# a, b, c = int(input()), int(input()), int(input())
+#
+# # вызываем функцию
+# print(is_valid_triangle(a, b, c))
+
+
+# Напишите функцию is_prime(num),
+# которая принимает в качестве аргумента натуральное число
+# и возвращает значение True если число является простым и False в противном случае.
+
 # объявление функции
-def is_valid_triangle(side1, side2, side3):
-    if a + b > c and a + c > b and b + c > a:
-        return True
+def is_prime(num):
+    flag = True
+    for i in range(2, num):
+        if num % i == 0:  # если исходное число делится на какое-либо отличное от 1 и самого себя
+            flag = False
+    if num == 1:
+        return False
+    elif flag == True:
+        return  True
     else:
         return False
 
 # считываем данные
-a, b, c = int(input()), int(input()), int(input())
+n = int(input())
 
 # вызываем функцию
-print(is_valid_triangle(a, b, c))
+print(is_prime(n))
