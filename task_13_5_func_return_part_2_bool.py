a0bdd0435fefbdb1bee9a912a6d567f289c406fa
Vitali Shnitko <stigmator565@gmail.com>
2022-12-08 20:52:53 +0300

    Added solution 8 task.

--- task_13_5_func_return_part_2_bool.py
+++ task_13_5_func_return_part_2_bool.py
@@ -187,3 +187,43 @@
 # print(is_correct_bracket(txt))
 
 
+# Напишите функцию is_password_good(password),
+# которая принимает в качестве аргумента строковое значение
+# пароля password и возвращает значение True если пароль
+# является надежным и False в противном случае.
+#
+# Пароль является надежным если:
+#
+# его длина не менее 88 символов;
+# он содержит как минимум одну заглавную букву (верхний регистр);
+# он содержит как минимум одну строчную букву (нижний регистр);
+# он содержит хотя бы одну цифру.
+
+# объявление функции
+def is_password_good(password):
+    if len(password) < 8:
+        return False
+    upp = False
+    low = False
+    dig = False
+    for i in range(0, len(password)):
+        if password[i].isupper():
+            upp = True
+        elif password[i].islower():
+            low = True
+        elif password[i].isdigit():
+            dig = True
+    if upp*low*dig == 1:
+        return True
+    else:
+        return False
+
+
+
+# считываем данные
+txt = input()
+
+# вызываем функцию
+print(is_password_good(txt))
+
+
