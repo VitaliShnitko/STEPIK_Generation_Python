f4726a20fa92bc9ce2f352b71c717de5ee2769d6
Vitali Shnitko <stigmator565@gmail.com>
2022-12-12 18:36:03 +0300

    Added solution 2 task.

--- task_13_6_func_return_part_3_return_value.py
+++ task_13_6_func_return_part_3_return_value.py
@@ -3,16 +3,37 @@
 # концов отрезка (x_1, y_1)(x) и (x_2, y_2)(x) и
 # возвращает координаты точки являющейся серединой данного отрезка.
 
+# # объявление функции
+# def get_middle_point(x1, y1, x2, y2):
+#     a = (x1+x2) / 2
+#     b = (y1+y2) / 2
+#     return a, b
+#
+# # считываем данные
+# x_1, y_1 = int(input()), int(input())
+# x_2, y_2 = int(input()), int(input())
+#
+# # вызываем функцию
+# x, y = get_middle_point(x_1, y_1, x_2, y_2)
+# print(x, y)
+
+
+# Напишите функцию get_circle(radius),
+# которая принимает в качестве аргумента радиус
+# окружности и возвращает два значения:
+# длину окружности и площадь круга, ограниченного данной окружностью.
+
+
+import math
 # объявление функции
-def get_middle_point(x1, y1, x2, y2):
-    a = (x1+x2) / 2
-    b = (y1+y2) / 2
-    return a, b
+def get_circle(radius):
+    c = 2 * math.pi * radius
+    s = math.pi * (radius ** 2)
+    return c, s
 
 # считываем данные
-x_1, y_1 = int(input()), int(input())
-x_2, y_2 = int(input()), int(input())
+r = float(input())
 
 # вызываем функцию
-x, y = get_middle_point(x_1, y_1, x_2, y_2)
-print(x, y)
+length, square = get_circle(r)
+print(length, square)
